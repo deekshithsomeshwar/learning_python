4ccdb3668a359d123521ae25e78bd065e997c25a
deekshithsomeshwar <deekshith_someshwar@live.com>
2018-07-02 14:00:47 +0530

    fixed bugs for title and uppercase

--- Dictionary/app1.py
+++ Dictionary/app1.py
@@ -4,24 +4,30 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
-    w = w.lower()
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
-        if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
-        elif yn == "N":
-            return "The word doesn't exist. Please double check it."
-        else:
-            return "We didn't understand your entry."
-    else:
-        return "The word doesn't exist. Please double check it."
+	w = w.lower()
+	if w in data:
+	    return data[w]
+	elif w.title() in data:
+		return data[w.title()]
+	elif w.upper() in data:
+		return data[w.upper()]
+	elif len(get_close_matches(w, data.keys())) >0:
+		yn = raw_input("did you mean %s instead, press Y for yes or N for no" % get_close_matches(w, data.keys())[0])
+		if yn == "Y":
+		    return data[get_close_matches(w,data.keys())[0]]
+		elif yn == "N":
+		    return "Not sure if you entered the right word"
+		else:
+			return "Try again"
+	else:
+		return "exiting"
 
-word = input("Enter word: ")
+word = raw_input("enter the word: ")
 output = translate(word)
 if type(output) == list:
     for item in output:
-        print(item)
+	  print(item)
 else:
-    print(output)
+	print(output)
+	
+	
